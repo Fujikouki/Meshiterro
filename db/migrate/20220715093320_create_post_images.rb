24b976664dc9cf5ed958bcf5f8f8bf35ddec9863
EC2 Default User <ec2-user@ip-172-31-15-81.ec2.internal>
2022-07-20 11:00:48 +0000

    一覧表示完成

--- db/migrate/20220715093320_create_post_images.rb
+++ db/migrate/20220715093320_create_post_images.rb
@@ -2,8 +2,13 @@ class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
 
+      t.string :shop_name
+      t.text :captino
+      t.integer :user_id
+
       t.timestamps
-      
+
+
     end
   end
 end
